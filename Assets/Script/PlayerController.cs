9f24fd6cc60df162f2c6cb587b63ec2ab0b02a1b
yuma140902 <yuma140902@gmail.com>
2022-03-29 12:07:31 +0900

    第2回

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
+    public float RotationSpeed;
+    public GameObject Camera;
+
+    float pitch;
+    float yaw;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        pitch = Camera.transform.localRotation.eulerAngles.x;
+        yaw = Camera.transform.localRotation.eulerAngles.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.W)) {
+            this.transform.position += this.transform.forward * Speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            this.transform.position -= this.transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += this.transform.right * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position -= this.transform.right * Speed * Time.deltaTime;
+        }
+
+        float mouseVertical = Input.GetAxis("Mouse Y");
+        pitch += -mouseVertical * RotationSpeed;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        yaw += mouseHorizontal * RotationSpeed;
+
+        Camera.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        this.transform.localRotation = Quaternion.Euler(0, yaw, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
